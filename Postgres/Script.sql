7fc686f60dc35eb810946d594454459baea07e6a
Jean Carlos Galhardi <jeancarlosgalhardi@gmail.com>
2024-09-22 09:17:30 -0300

    Update Script.sql

--- Postgres/Script.sql
+++ Postgres/Script.sql
@@ -12,7 +12,13 @@ insert into sensor (id_sensor, descricao) values(1, 'Sensor 1');
 
 -- CRIAÇÃO DA TABELA NIVEL
 create table nivel(
-  datahora date not null,
+  datahora timestamp not null,
   id_sensor int not null references sensor(id_sensor),
   valor int not null  
 );
+
+-- EXEMPLO DE INSERT
+insert into nivel (datahora, id_sensor, valor) values (now(), 1, 10);
+
+-- EXEMPLO DE SELECT
+select to_char(datahora, 'dd/mm/yyyy hh24:mi'), id_sensor, valor from nivel;
