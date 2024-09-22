e7c4d7ea57b4fb86935ea40a2239a07412e60d69
Jean Carlos Galhardi <jeancarlosgalhardi@gmail.com>
2024-09-22 08:59:48 -0300

    Sensor Ultrassônico e Buzzer
    Funcionalidade leitura do sensor e acionamento do buzzer caso o volume de água está proximo.

---
+++ ESP32/Code_ESP32/Code_ESP32.ino
@@ -0,0 +1,43 @@
+// Biblioteca no Sensor
+#include "Ultrasonic.h"
+
+// Intervalo entre as leituras
+#define INTERVALO 1000
+
+// Definindo os pinos do Sensor
+#define PIN_TRIGGER 18
+#define PIN_ECHO 19
+#define PIN_BUZZER 21
+
+// Variável que armazenará a distância lida pelo Sensor
+unsigned int distancia = 0;
+
+// Inicializa o Sensor 
+HC_SR04 sensor(PIN_TRIGGER, PIN_ECHO);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_BUZZER,     OUTPUT);
+  Serial.println("Inicializando dispositivo");
+}
+
+void ligarBuzzer(){
+  digitalWrite(PIN_BUZZER,HIGH);
+}
+
+void desligaBuzzer(){
+   digitalWrite(PIN_BUZZER,LOW);
+}
+
+void loop() {
+distancia = sensor.distance();  
+
+  if (distancia < 5){
+    ligarBuzzer();
+  } else {
+    desligaBuzzer();
+  }
+
+  Serial.println(distancia);
+  delay(INTERVALO);
+}
